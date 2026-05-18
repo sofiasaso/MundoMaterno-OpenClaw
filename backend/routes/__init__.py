4c33a4b274b23e8da5542b987bad353cdc033338
sofiasaso <121638526+sofiasaso@users.noreply.github.com>
2026-05-18 18:33:58 -0500

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,1 +1,4 @@
-
+from .products import router as products_router
+from .metrics import router as metrics_router
+from .alerts import router as alerts_router
+from .scraping import router as scraping_router
