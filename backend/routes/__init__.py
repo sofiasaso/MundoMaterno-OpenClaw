7b906bd883928f603455242ba4f5278ee9d3121d
sofiasaso <121638526+sofiasaso@users.noreply.github.com>
2026-05-18 20:29:19 -0500

    Update __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,6 +1,1 @@
-print(">>> ROUTES INIT EJECUTADO")
 
-from .products import router as products_router
-from .metrics import router as metrics_router
-from .alerts import router as alerts_router
-from .scraping import router as scraping_router
